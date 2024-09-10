211ac7ed7333b41fb2a16c57167d59a49de14349
jiayuans <jiayuans422@gmail.com>
2024-09-10 12:37:10 -0600

    new

--- out.rec3.R
+++ out.rec3.R
@@ -16,7 +16,7 @@ Flag<-rep(NA,I)
 b0.mean<-rep(NA,I)
 b1.mean<-rep(NA,I)
 a.mean<-rep(NA,I)
-ph.mean<-rep(NA,I)
+ga.mean<-rep(NA,I)
 v.mean<-rep(NA,I)
 
 for(i in 1:I){ 
@@ -24,11 +24,17 @@ Flag[i] <- ifelse(max(data_frames[[i]][,12])<1.1,1,0)
 b0.mean[i] <-data_frames[[i]][1,5] 
 b1.mean[i] <-data_frames[[i]][2,5] 
 a.mean[i] <-data_frames[[i]][3,5] 
-ph.mean[i] <-data_frames[[i]][4,5] 
-v.mean[i] <-mean(data_frames[[i]][5:404,5])
+ga.mean[i] <-data_frames[[i]][4,5] 
+u.tau.inv.mean[i] <-data_frames[[i]][5,5] 
+w.tau.inv.mean[i] <-data_frames[[i]][6,5] 
+u.mean[i] <-mean(data_frames[[i]][7:406,5])
+v.mean[i] <-mean(data_frames[[i]][407:806,5])
+w.mean[i] <-mean(data_frames[[i]][807:1206,5])
+u.tau.mean[i] <-data_frames[[i]][1207,5] 
+w.tau.mean[i] <-data_frames[[i]][1208,5]
 }
 
-Sim.results=cbind(Flag,b0.mean,b1.mean,a.mean,ph.mean,v.mean)
+Sim.results=cbind(Flag,b0.mean,b1.mean,a.mean,ga.mean,u.tau.inv.mean,w.tau.inv.mean,u.mean,v.mean,w.mean,u.tau.mean,w.tau.mean)
 table(Flag)
 Sim.results.1 <- subset(Sim.results,Flag==1)
 round(colMeans(Sim.results.1),2)
