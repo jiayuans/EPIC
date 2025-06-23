1809e094e5eaf8abea94d60b9c1cae9394ad6978
Jiayuan Shi <jiayu@MacBook-Pro-2.local>
2025-06-23 00:27:35 -0600

    mix JM

---
+++ out.mixJM.R
@@ -0,0 +1,69 @@
+setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM")
+
+###########################################################################
+# Read csv files
+text <- list.files(pattern="mixJM.result.")
+num <- as.numeric(unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[3]])))
+
+data_frames <- lapply(num, function(i) {
+  file_name <- paste0("mixJM.result.", i, ".csv") 
+  read.csv(file_name)
+})
+
+I=length(data_frames)
+
+Flag<-rep(NA,I)
+B11.mean<-rep(NA,I)
+B12.mean<-rep(NA,I)
+c10.mean<-rep(NA,I)
+c20.mean<-rep(NA,I)
+c1.mean<-rep(NA,I)
+c2.mean<-rep(NA,I)
+cp1.mean<-rep(NA,I)
+pi1.mean<-rep(NA,I)
+pi2.mean<-rep(NA,I)
+u10.mean<-rep(NA,I)
+u10.tau.inv.mean<-rep(NA,I)
+u20.mean<-rep(NA,I)
+u20.tau.inv.mean<-rep(NA,I)
+u11.mean<-rep(NA,I)
+u11.tau.inv.mean<-rep(NA,I)
+u12.mean<-rep(NA,I)
+u12.tau.inv.mean<-rep(NA,I)
+u21.mean<-rep(NA,I)
+u21.tau.inv.mean<-rep(NA,I)
+cp1.mu.mean<-rep(NA,I)
+cp1.tau.mean<-rep(NA,I)
+
+for(i in 1:I){ 
+  Flag[i] <- ifelse(max(data_frames[[i]][,12], na.rm = TRUE)<1.1,1,0)
+  B11.mean[i] <- data_frames[[i]][1,5] 
+  B12.mean[i] <- data_frames[[i]][2,5] 
+  c10.mean[i] <-data_frames[[i]][3,5] 
+  c20.mean[i] <-data_frames[[i]][4,5] 
+  c1.mean[i] <-data_frames[[i]][5,5] 
+  c2.mean[i] <-data_frames[[i]][6,5] 
+  cp1.mean[i] <-mean(data_frames[[i]][7:406,5])
+  pi1.mean[i] <-data_frames[[i]][407,5] 
+  pi2.mean[i] <-data_frames[[i]][408,5] 
+  u10.mean[i] <-mean(data_frames[[i]][809:1208,5])
+  u10.tau.inv.mean[i] <-data_frames[[i]][2809,5]
+  u20.mean[i] <-mean(data_frames[[i]][1209:1608,5])
+  u20.tau.inv.mean[i] <-data_frames[[i]][2810,5]
+  u11.mean[i] <-mean(data_frames[[i]][1609:2008,5])
+  u11.tau.inv.mean[i] <-data_frames[[i]][2811,5]
+  u12.mean[i] <-mean(data_frames[[i]][2009:2408,5])
+  u12.tau.inv.mean[i] <-data_frames[[i]][2812,5]
+  u21.mean[i] <-mean(data_frames[[i]][2409:2808,5])
+  u21.tau.inv.mean[i] <-data_frames[[i]][2813,5]
+  cp1.mu.mean[i] <-data_frames[[i]][2819,5]
+  cp1.tau.mean[i] <-data_frames[[i]][2820,5]
+}
+
+Sim.results=cbind(Flag,B11.mean,B12.mean,cp1.mean,c10.mean,c20.mean,c1.mean,c2.mean,pi1.mean,pi2.mean,
+                  u10.tau.inv.mean,u10.mean,u20.tau.inv.mean,u20.mean,u11.tau.inv.mean,u11.mean,u12.tau.inv.mean,u12.mean,u21.tau.inv.mean,u21.mean,
+                  cp1.mu.mean,cp1.tau.mean)
+table(Flag)
+Sim.results.1 <- subset(Sim.results,Flag==1)
+round(colMeans(Sim.results.1),2)
+round(colMeans(Sim.results),2)
--- sim_data.mixJM.R
+++ sim_data.mixJM.R
@@ -20,9 +20,21 @@ length(id)
 
 t<-round(first.tt)
 tt<-round(last.tt)
+k.pa<-(tt-t)*4
+kk=max(k.pa)
 
 ###set number of iterations#################################
 I=201
+
+###############set true values#########################################
+c10=-3.3
+c20=-2.6
+c1=0.3
+c2=0.3
+c3=-0.04
+Verror=1
+cp.true=13
+pi <- c(0.55, 0.45)
 
 #############################################################
 set.seed(123)
@@ -57,9 +69,10 @@ NHPP<-function(a,b,T){
 # Output: A dataset with variables
 # 		 id, xi (treatment),Tei, time, status
 # -------------- Building the simulated poisson data -----
-poisson.d <- function(alpha,beta,beta0,x,ph,TTei){
+poisson1.d <- function(alpha,beta,beta0,x,ga0,ga,TTei){
   le <- length(x)
-  vi <- ifelse(rep(ph,le)==rep(0,le),rep(1,le),rgamma(le,shape=1/ph, scale=ph))
+  c_0i <- rnorm(le,0,1) 
+  vi <- exp(ga0*b_10i+c_0i+ga*cp_i)
   
   times <- NHPP(b=vi[1]*exp(beta*x[1])*exp(beta0),a=alpha,T=TTei[1])
   start <-  times[,1]
@@ -87,13 +100,70 @@ poisson.d <- function(alpha,beta,beta0,x,ph,TTei){
   return(data.frame(id,xi,Tei,n.rec,start,stop,status))
 }
 
-p1r <- 0.5  # for example
+poisson2.d <- function(alpha,beta,beta0,x,ga0,TTei){
+  le <- length(x)
+  c_0i <- rnorm(le,0,1) 
+  vi <- exp(ga0*b_20i+c_0i)
+  
+  times <- NHPP(b=vi[1]*exp(beta*x[1])*exp(beta0),a=alpha,T=TTei[1])
+  start <-  times[,1]
+  stop <- times[,2]
+  status <- times[,3]
+  n.rec <- times[,4]
+  id <- rep(1,length(stop))
+  xi <- rep(x[1],length(stop))
+  Tei <- rep(TTei[1],length(stop))
+  for (i in 2:length(x)){
+    times2 <- NHPP(b=vi[i]*exp(beta0+beta*x[i]),a=alpha,T=TTei[i]) 
+    start2 <-  times2[,1]
+    stop2 <- times2[,2]
+    status2 <- times2[,3]
+    n.rec2 <- times2[,4]
+    id <- c(id,rep(i,length(stop2)))
+    xi <- c(xi,rep(x[i],length(stop2)))
+    Tei <- c(Tei,rep(TTei[i],length(stop2)))
+    
+    start <- c(start,start2)
+    stop <- c(stop,stop2)
+    status <- c(status,status2)
+    n.rec <- c(n.rec,n.rec2)
+  }
+  return(data.frame(id,xi,Tei,n.rec,start,stop,status))
+}
+
+p1r <- 0.8  # for example
 
 #######################################################;
 for (r in 2:I){
   X1=c(rep(1,N/2),rep(0,N/2))
-  simdat.pe1 <- poisson.d(alpha=1,beta=0.8,beta0=-2.5,x=X1,ph=1,TTei=tt-0.25)
-  simdat.pe2 <- poisson.d(alpha=1,beta=2,beta0=0.5,x=X1,ph=.5,TTei=tt-0.25)  
+  
+  b_10i<-rnorm(N,0,1) 
+  b_20i<-rnorm(N,0,1)
+  cp_i<-rnorm(N,cp.true,1) 
+  z <- sample(1:2, size = N, prob = pi, replace = TRUE)
+  
+  I<-matrix(NA, nrow=N, ncol=kk, byrow=TRUE)
+  p1<-matrix(NA, nrow=N, ncol=kk, byrow=TRUE)
+  p2<-matrix(NA, nrow=N, ncol=kk, byrow=TRUE)
+  p<-matrix(NA, nrow=N, ncol=kk, byrow=TRUE)
+  Y<-matrix(NA, nrow=N, ncol=kk, byrow=TRUE)
+  X<-matrix(NA, nrow=N, ncol=kk, byrow=TRUE)
+  for (i in 1:N){
+    X[i,1:k.pa[i]]<-c(seq(t[i],tt[i]-0.25,0.25))
+  }
+  
+  for (i in 1:N){
+    for (j in 1:k.pa[i]){
+      I[i,j]<-ifelse(X[i,j] < cp_i[i],-1,1)
+      p1[i,j]=exp(c10+c1*(X[i,j]-cp_i[i])+c2*(X[i,j]-cp_i[i])*I[i,j]+c3*X1[i]+b_10i[i])/(1+exp(c10+c1*(X[i,j]-cp_i[i])+c2*(X[i,j]-cp_i[i])*I[i,j]+c3*X1[i]+b_10i[i]))
+      p2[i,j]=exp(c20+(c1-c2)*X[i,j]+c3*X1[i]+b_20i[i])/(1+exp(c20+(c1-c2)*X[i,j]+c3*X1[i]+b_20i[i]))
+      p[i,j] <- if (z[i] == 1) p1[i,j] else  p2[i,j]
+      Y[i,j]=rbinom(Verror, 1, p[i,j])
+    }
+  }
+  
+  simdat.pe1 <- poisson1.d(alpha=1.8,beta=0.4,beta0=-3.7,x=X1,ga0=1.4,ga=-0.1,TTei=tt-0.25)
+  simdat.pe2 <- poisson2.d(alpha=1.8,beta=0.1,beta0=-3.5,x=X1,ga0=0.25,TTei=tt-0.25)  
   
   model_source <- rbinom(N, 1, p1r)  # 1 = from pe1, 0 = from pe2
   subject_ids_pe1 <- unique(simdat.pe1$id)
@@ -110,9 +180,17 @@ for (r in 2:I){
     dat_i$id <- i  # Reset id to keep it consistent
     simdat.pe <- rbind(simdat.pe, dat_i)
   }
+
+  X_df <- as.data.frame(X)
+  filename <- paste0("mixJM.X_data.", r-2, ".csv")
+  write.csv(X_df, file = filename, row.names = FALSE)
+  
+  Y_df <- as.data.frame(Y)
+  filename <- paste0("mixJM.Y_data.", r-2, ".csv")
+  write.csv(Y_df, file = filename, row.names = FALSE)
   
   simdat.pe_df <- as.data.frame(simdat.pe)
-  filename <- paste0("mixRM.rec_data.", r-2, ".csv")
+  filename <- paste0("mixJM.rec_data.", r-2, ".csv")
   write.csv(simdat.pe_df, file = filename, row.names = FALSE)
 } 
 
