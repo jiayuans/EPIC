f96247f9fcdf85f7202ca2abebb78d26a46ff54a
Jiayuan Shi <jiayu@MacBook-Pro-4.local>
2025-09-23 21:13:21 -0600

    1rcp1

--- Simulation_jointmodel_1rcp1.R
+++ Simulation_jointmodel_1rcp1.R
@@ -25,9 +25,9 @@ X1=c(rep(1,N/2),rep(0,N/2))
 set.seed(123)
 
 #############################################################
-X <- as.matrix(read.csv(list.files(pattern="X_data_1rcp.")))
-Y <- as.matrix(read.csv(list.files(pattern="Y_data_1rcp.")))
-simdat.pe00 <- as.data.frame(read.csv(list.files(pattern="sim.pe_data_1rcp.")))
+X <- as.matrix(read.csv(list.files(pattern="X_data_1rcp1.")))
+Y <- as.matrix(read.csv(list.files(pattern="Y_data_1rcp1.")))
+simdat.pe00 <- as.data.frame(read.csv(list.files(pattern="sim.pe_data_1rcp1.")))
 #############################################################
 
 tt<-tt-0.25
@@ -124,7 +124,7 @@ model {
   B2 <-c[1]+c[2]
   u.tau.inv <- 1/u.tau  ## variance 
   a ~ dgamma(0.01,0.01)
-  b0 ~ dnorm(-2, 0.02)  
+  b0 ~ dnorm(-1, 0.02)  
   b ~ dnorm(0,0.0001)
 	ga ~ dnorm(0,0.01)
 	ga1 ~ dnorm(0,0.01)
@@ -138,18 +138,18 @@ model {
                            X1=X1, k.pe=k.pe, time.t0=time.t0, time.tau=time.tau, Ti=Ti)) 
   ##initial Values
   inits1 <- dump.format(list(c0=-3, c=c(0.3,0.3,-0.05), u.tau=1, cp1.mu=15, cp1.tau=1, 
-                             b0=-2, b=0.2, a=1.8, w.tau=1, ga=0.3, ga1=-0.05,
+                             b0=-1, b=0.2, a=1.8, w.tau=1, ga=0.3, ga1=-0.05,
                              .RNG.name="base::Super-Duper", .RNG.seed=1))
   inits2 <- dump.format(list(c0=-3.1, c=c(0.3,0.3,-0.05)+0.01, u.tau=1, cp1.mu=15.1, cp1.tau=1, 
-                             b0=-2.1, b=0.21, a=1.8, w.tau=1, ga=0.31, ga1=-0.04,
+                             b0=-1.1, b=0.21, a=1.8, w.tau=1, ga=0.31, ga1=-0.04,
                              .RNG.name="base::Super-Duper", .RNG.seed=2))
 
   #### Run the model and produce plots
-  res <- run.jags(model=modelrancp, burnin=1000, sample=5000, 
+  res <- run.jags(model=modelrancp, burnin=1000, sample=3000, 
                   monitor=c("B1","B2","cp1","c0","c","u.tau.inv",
                             "b0","b","a","ga","ga1","w.tau.inv", "cp1.mu","cp1.tau.inv","u","v","w",
                             "u.tau","w.tau","ll.a","ll.e","dev.a","dev.e"), 
-                  data=data, n.chains=2, method = "parallel",inits=c(inits1,inits2), thin=2)
+                  data=data, n.chains=2, method = "parallel",inits=c(inits1,inits2), thin=4)
   
   #res <- run.jags(model=modelrancp, burnin=20000, sample=4000/10000/5000, 
   #               monitor=c("B1","B2","cp1","c0","c","u.tau.inv",
@@ -159,7 +159,7 @@ model {
   
   summary <- summary(res)
   result_df <- as.data.frame(summary)
-  text <- list.files(pattern="X_data_1rcp.")
+  text <- list.files(pattern="X_data_1rcp1.")
   num <- unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[2]]))
   write.csv(result_df, paste0("result_1rcp1.",num,".csv"))
   save(res, file=paste0("res_1rcp1.",num,".RData"))
