1ccab7de3af05af46ca2b72ab6d16524775a24d1
jiayuans <jiayu@MacBook-Pro-8.local>
2026-02-25 20:20:01 -0700

    new JM1

--- out.mixJM.R
+++ out.mixJM.R
@@ -1,14 +1,14 @@
 setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM_020225")
 setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM_021125")
-setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM3_022425")
+setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM1_022525")
 
 ###########################################################################
 # Read csv files
-text <- list.files(pattern="mixJM.result3.")
+text <- list.files(pattern="mixJM.result1.")
 num <- as.numeric(unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[3]])))
 
 data_frames <- lapply(num, function(i) {
-  file_name <- paste0("mixJM.result3.", i, ".csv") 
+  file_name <- paste0("mixJM.result1.", i, ".csv") 
   read.csv(file_name)
 })
 
