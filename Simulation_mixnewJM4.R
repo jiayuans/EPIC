a13f00ce213e8b99cb403c1f3b7f19d6b1516064
jiayuans <jiayu@MacBook-Pro-8.local>
2026-03-16 23:07:29 -0600

    new

--- Simulation_mixnewJM4.R
+++ Simulation_mixnewJM4.R
@@ -5,7 +5,7 @@ library(runjags)
 library(tidyverse)
 library(mcmcplots)
 
-long.time <- read.csv("long.data_new800.csv")
+long.time <- read.csv("long.data_new600.csv")
 first.tt <- long.time[,2]
 last.tt <- long.time[,3]
 
@@ -26,9 +26,9 @@ alpha = c(1,1)
 set.seed(123)
 
 #############################################################
-X <- as.matrix(read.csv(list.files(pattern="mixJM.X_newdata.")))
-Y <- as.matrix(read.csv(list.files(pattern="mixJM.Y_newdata.")))
-simdat.pe00 <- as.data.frame(read.csv(list.files(pattern="mixJM.rec_newdata.")))
+X <- as.matrix(read.csv(list.files(pattern="mixJM.X_newdata2.")))
+Y <- as.matrix(read.csv(list.files(pattern="mixJM.Y_newdata2.")))
+simdat.pe00 <- as.data.frame(read.csv(list.files(pattern="mixJM.rec_newdata2.")))
 #############################################################
 
 tt<-tt-0.25
@@ -114,6 +114,9 @@ model {
     u2[i] ~ dnorm(0, u.tau2)
     cp1[i] ~ dnorm(cp1.mu, cp1.tau)
 
+    # Center cp1 inside v1
+    cp1c[i] <- cp1[i] - cp1.mu
+    
     # PA likelihood contribution
     L.a[i]  <- prod( (p2[i,1:k.pa[i]]^Y[i,1:k.pa[i]]) * ((1-p2[i,1:k.pa[i]])^(1-Y[i,1:k.pa[i]])) )
     ll.a[i] <- log(L.a[i])
@@ -121,15 +124,15 @@ model {
     # ---- PE (NHPP / Weibull process) ----
     w1[i] ~ dnorm(0, w.tau1)
     w2[i] ~ dnorm(0, w.tau2)
-    v1[i] <- exp(ga10*u1[i] + w1[i] + ga11*cp1[i])
+    v1[i] <- exp(ga10*u1[i] + w1[i] + ga11*cp1c[i]) # v1[i] <- exp(ga10*u1[i] + w1[i] + ga11*cp1[i])
     v2[i] <- exp(ga20*u2[i] + w2[i])
 
     # Baseline intensity pieces at event times
     for(j in 1:max.count){
-      lambda0[i,j] <- a * (Ti2[i,j])^(a-1)
-
-      lambda1[i,j] <- lambda0[i,j] * v1[i] * exp(b10 + b[1]*X1[i])
-      lambda2[i,j] <- lambda0[i,j] * v2[i] * exp(b20 + b[2]*X1[i])
+      lambda10[i,j] <- a1 * (Ti2[i,j])^(a1-1)
+      lambda20[i,j] <- a2 * (Ti2[i,j])^(a2-1)
+      lambda1[i,j] <- lambda10[i,j] * v1[i] * exp(b10 + b[1]*X1[i])
+      lambda2[i,j] <- lambda20[i,j] * v2[i] * exp(b20 + b[2]*X1[i])
 
       loghaz1[i,j] <- E[i,j] * log(lambda1[i,j])
       loghaz2[i,j] <- E[i,j] * log(lambda2[i,j])
@@ -139,27 +142,18 @@ model {
     # sum log lambda(t_j) - b*(tau^a - t0^a)
     # where b = v*exp(b0+bX)
     logL1[i] <- sum(loghaz1[i,1:max.count]) -
-      v1[i] * exp(b10 + b[1]*X1[i]) * (time.tau[i]^a - time.t0[i]^a)
+      v1[i] * exp(b10 + b[1]*X1[i]) * (time.tau[i]^a1 - time.t0[i]^a1)
 
     logL2[i] <- sum(loghaz2[i,1:max.count]) -
-      v2[i] * exp(b20 + b[2]*X1[i]) * (time.tau[i]^a - time.t0[i]^a)
+      v2[i] * exp(b20 + b[2]*X1[i]) * (time.tau[i]^a2 - time.t0[i]^a2)
 
     # Mixture over PE components
-    maxlogL[i] <- max(logL1[i], logL2[i])
-
-    ll.e[i] <- maxlogL[i] + log(
-      pi.r[1] * exp(logL1[i] - maxlogL[i]) +
-      pi.r[2] * exp(logL2[i] - maxlogL[i])
-    )
+    z.r[i] ~ dcat(pi.r[1:2]) 
+    ll.e[i] <- equals(z.r[i],1) * logL1[i] + equals(z.r[i],2) * logL2[i]
 
     # zeros trick for custom likelihood
     phi[i] <- max(-ll.e[i] + 1000000, 0)
     zeros[i] ~ dpois(phi[i])
-
-    # Optional classification draw (post-processing)
-    prob_class[i,1] <- pi.r[1] * exp(logL1[i] - ll.e[i])
-    prob_class[i,2] <- pi.r[2] * exp(logL2[i] - ll.e[i])
-    z.r[i] ~ dcat(prob_class[i,1:2])
   }
 
   # -------------------------
@@ -177,47 +171,45 @@ model {
   pi.r[1:2] ~ ddirch(alpha.r[])
 
   # PA intercept ordering: c20 > c10
-  c10 ~ dnorm(0,0.0001)
-  c20 ~ dnorm(0,0.0001) T(c10, )
+  c10 ~ dnorm(0,0.001)
+  c20 ~ dnorm(0,0.001) T(c10, )
 
   for (k in 1:3){
-    c[k] ~ dnorm(0,0.0001)
+    c[k] ~ dnorm(0,0.001)
   }
   B1 <- c[1] - c[2]
   B2 <- c[1] + c[2]
 
-  u.tau1 ~ dgamma(16,4) # u.tau1 ~ dgamma(0.001,0.001)
+  u.tau1 ~ dgamma(0.001,0.001)
   u.tau.inv1 <- 1/u.tau1
 
-  u.tau2 ~ dgamma(16,4) # u.tau2 ~ dgamma(0.001,0.001)
+  u.tau2 ~ dgamma(0.001,0.001)
   u.tau.inv2 <- 1/u.tau2
 
   cp1.mu ~ dnorm(0,0.01)
   cp1.tau ~ dgamma(0.01,0.01)
   cp1.tau.inv <- 1/cp1.tau
 
-  a ~ dgamma(0.01,0.01)
-
+  a1 ~ dgamma(0.01,0.01)
+  a2 ~ dgamma(0.01,0.01)
   # PE ordering: b10 < b20
-  b20_raw ~ dnorm(0, 0.25)
-  delta_b ~ dnorm(0, 0.25) T(0,)
+  b20_raw ~ dnorm(0, 0.001)
+  delta_b ~ dnorm(0, 0.001) T(0,)
   b10 <- b20_raw - delta_b
   b20 <- b20_raw
-  # b10 ~ dnorm(0,0.25)
-  # b20 ~ dnorm(0,0.25)
 
   for (p in 1:2){
-    b[p] ~ dnorm(0,0.25)
+    b[p] ~ dnorm(0,0.001)
   }
 
-  ga10 ~ dnorm(0,0.0001)
-  ga20 ~ dnorm(0,0.0001)
-  ga11 ~ dnorm(0,0.0001)
+  ga10 ~ dnorm(0,0.01)
+  ga20 ~ dnorm(0,0.01)
+  ga11 ~ dnorm(0,0.01)
 
-  w.tau1 ~ dgamma(2,2)
+  w.tau1 ~ dgamma(0.001, 0.001) 
   w.tau.inv1 <- 1/w.tau1
 
-  w.tau2 ~ dgamma(2,2)
+  w.tau2 ~ dgamma(0.001, 0.001) 
   w.tau.inv2 <- 1/w.tau2
 }
 "
@@ -225,11 +217,11 @@ model {
 ####Observed DATA
 data <- dump.format(list(N=N, X=X, Y=Y, X1=X1,k.pa=k.pa,max.count=max.count, time.t0=time.t0, time.tau=time.tau, Ti2=Ti2, E=E, alpha=alpha, alpha.r=alpha.r)) 
 ###initial Values
-inits1 <- dump.format(list(c10=-3.3, c20=-2.6, c=c(0.3,0.3,-0.05), pi=c(0.55,0.45), pi.r=c(0.6,0.4), u.tau1=4,u.tau2=4, cp1.mu=14, cp1.tau=0.4,
-                           b20_raw=-2, delta_b=2, b=c(0.2,0.3), a=1.8, w.tau1=11.1, w.tau2=11.1, ga10=1.5, ga20=-0.4, ga11=-0.1,
+inits1 <- dump.format(list(c10=-3.3, c20=-2.6, c=c(0.3,0.3,-0.05), pi=c(0.55,0.45), pi.r=c(0.6,0.4), u.tau1=4,u.tau2=4, cp1.mu=14, cp1.tau=1,
+                           b20_raw=-2, delta_b=2, b=c(0.2,0.3), a1=1.8,a2=1.6, w.tau1=11.1, w.tau2=11.1, ga10=1, ga20=-0.2, ga11=-0.1,
                            .RNG.name="base::Super-Duper", .RNG.seed=1)) 
-inits2 <- dump.format(list(c10=-3.2, c20=-2.5, c=c(0.3,0.3,-0.05)+0.01, pi=c(0.56,0.44), pi.r=c(0.59,0.41), u.tau1=3.6,u.tau2=4.4, cp1.mu=14.1, cp1.tau=0.5,
-                           b20_raw=-1.9, delta_b=2.2, b=c(0.2,0.3)+0.1, a=1.75, w.tau1=10, w.tau2=12, ga10=1.4, ga20=-0.3, ga11=-0.08,
+inits2 <- dump.format(list(c10=-3.2, c20=-2.5, c=c(0.3,0.3,-0.05)+0.01, pi=c(0.56,0.44), pi.r=c(0.59,0.41), u.tau1=3.6,u.tau2=4.4, cp1.mu=14.1, cp1.tau=0.9,
+                           b20_raw=-1.9, delta_b=2.2, b=c(0.2,0.3)+0.1, a1=1.75,a2=1.55, w.tau1=10, w.tau2=12, ga10=1.1, ga20=-0.1, ga11=-0.08,
                            .RNG.name="base::Super-Duper", .RNG.seed=2))
 
 #### Run the model and produce plots
@@ -237,7 +229,7 @@ res <- run.jags(model=modelrancp, burnin=10000, sample=5000,
                 monitor=c("B1","B2","c10", "c20","c", "cp1",
                           "pi","pi.r","u.tau.inv1","u.tau.inv2", "u.tau1","u.tau2",
                           "cp1.mu","cp1.tau.inv","cp1.tau",
-                          "b10","b20","b", "a","ga10","ga20","ga11",
+                          "b10","b20","b", "a1","a2","ga10","ga20","ga11",
                           "w.tau1","w.tau2","w.tau.inv1","w.tau.inv2","b20_raw","delta_b",
                           "ll.a","ll.e","dev.a","dev.e"), 
                 data=data, n.chains=2, method = "parallel", inits=c(inits1,inits2), thin=15)
@@ -245,7 +237,7 @@ res <- run.jags(model=modelrancp, burnin=10000, sample=5000,
 summary <- summary(res)
 summary
 result_df <- as.data.frame(summary)
-text <- list.files(pattern="mixJM.X_newdata.")
+text <- list.files(pattern="mixJM.X_newdata2.")
 num <- unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[3]]))
 write.csv(result_df, paste0("mixJM.newresult4.",num,".csv"))
 save(res, file=paste0("mixJM.newres4.",num,".RData"))
--- out.mixJM.R
+++ out.mixJM.R
@@ -1,14 +1,109 @@
 setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM_020225")
 setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM_021125")
-setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixnewJM5_031325")
+setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixnewJM3_031625")
 
 ###########################################################################
 # Read csv files
-text <- list.files(pattern="mixJM.newresult5.")
+text <- list.files(pattern="mixJM.newresult3.")
 num <- as.numeric(unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[3]])))
 
 data_frames <- lapply(num, function(i) {
-  file_name <- paste0("mixJM.newresult5.", i, ".csv") 
+  file_name <- paste0("mixJM.newresult3.", i, ".csv") 
+  read.csv(file_name)
+})
+
+I=length(data_frames)
+I
+
+Flag<-rep(NA,I)
+B1.mean<-rep(NA,I)
+B2.mean<-rep(NA,I)
+c10.mean<-rep(NA,I)
+c20.mean<-rep(NA,I)
+c1.mean<-rep(NA,I)
+c2.mean<-rep(NA,I)
+c3.mean<-rep(NA,I)
+cp1.mean<-rep(NA,I)
+pi1.mean<-rep(NA,I)
+pi2.mean<-rep(NA,I)
+pi1r.mean<-rep(NA,I)
+pi2r.mean<-rep(NA,I)
+#u1.mean<-rep(NA,I)
+u1.tau.inv.mean<-rep(NA,I)
+#u2.mean<-rep(NA,I)
+u2.tau.inv.mean<-rep(NA,I)
+cp1.mu.mean<-rep(NA,I)
+cp1.tau.mean<-rep(NA,I)
+
+b10.mean<-rep(NA,I)
+b20.mean<-rep(NA,I)
+b1.mean<-rep(NA,I)
+b2.mean<-rep(NA,I)
+a1.mean<-rep(NA,I)
+a2.mean<-rep(NA,I)
+ga10.mean<-rep(NA,I)
+ga20.mean<-rep(NA,I)
+ga11.mean<-rep(NA,I)
+#w1.mean<-rep(NA,I)
+w1.tau.inv.mean<-rep(NA,I)
+#w2.mean<-rep(NA,I)
+w2.tau.inv.mean<-rep(NA,I)
+
+for(i in 1:I){ 
+  Flag[i] <- ifelse(max(data_frames[[i]][,12], na.rm = TRUE)<1.3,1,0)
+  B1.mean[i] <- data_frames[[i]][1,5] 
+  B2.mean[i] <- data_frames[[i]][2,5] 
+  c10.mean[i] <-data_frames[[i]][3,5] 
+  c20.mean[i] <-data_frames[[i]][4,5] 
+  c1.mean[i] <-data_frames[[i]][5,5] 
+  c2.mean[i] <-data_frames[[i]][6,5] 
+  c3.mean[i] <-data_frames[[i]][7,5] 
+  cp1.mean[i] <-mean(data_frames[[i]][8:607,5])
+  pi1.mean[i] <-data_frames[[i]][608,5] 
+  pi2.mean[i] <-data_frames[[i]][609,5] 
+  pi1r.mean[i] <-data_frames[[i]][610,5] 
+  pi2r.mean[i] <-data_frames[[i]][611,5] 
+  #u1.mean[i] <-mean(data_frames[[i]][812:1611,5])
+  u1.tau.inv.mean[i] <-data_frames[[i]][612,5]
+  #u2.mean[i] <-mean(data_frames[[i]][1612:2011,5])
+  u2.tau.inv.mean[i] <-data_frames[[i]][613,5]
+  cp1.mu.mean[i] <-data_frames[[i]][616,5]
+  cp1.tau.mean[i] <-data_frames[[i]][617,5]
+  
+  b10.mean[i] <-data_frames[[i]][619,5] 
+  b20.mean[i] <-data_frames[[i]][620,5] 
+  b1.mean[i] <-data_frames[[i]][621,5] 
+  b2.mean[i] <-data_frames[[i]][622,5] 
+  a1.mean[i] <-data_frames[[i]][623,5] 
+  a2.mean[i] <-data_frames[[i]][624,5] 
+  ga10.mean[i] <-data_frames[[i]][625,5] 
+  ga20.mean[i] <-data_frames[[i]][626,5] 
+  ga11.mean[i] <-data_frames[[i]][627,5] 
+  #w1.mean[i] <-mean(data_frames[[i]][2027:2426,5])
+  w1.tau.inv.mean[i] <-data_frames[[i]][630,5]
+  #w2.mean[i] <-mean(data_frames[[i]][2427:2826,5])
+  w2.tau.inv.mean[i] <-data_frames[[i]][631,5]
+}
+
+Sim.results=cbind(Flag,B1.mean,B2.mean,cp1.mean,c10.mean,c20.mean,c1.mean,c2.mean,c3.mean,pi1.mean,pi2.mean,pi1r.mean,pi2r.mean,
+                  u1.tau.inv.mean,u2.tau.inv.mean,cp1.mu.mean,cp1.tau.mean,
+                  b10.mean,b20.mean,b1.mean,b2.mean,a1.mean,a2.mean,ga10.mean,ga20.mean,ga11.mean,w1.tau.inv.mean,w2.tau.inv.mean)
+table(Flag)
+Sim.results.1 <- subset(Sim.results,Flag==1)
+round(colMeans(Sim.results.1),2)
+round(colMeans(Sim.results),2)
+
+
+
+setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixnewJM3bu_031625")
+
+###########################################################################
+# Read csv files
+text <- list.files(pattern="mixJM.newresult3bu.")
+num <- as.numeric(unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[3]])))
+
+data_frames <- lapply(num, function(i) {
+  file_name <- paste0("mixJM.newresult3bu.", i, ".csv") 
   read.csv(file_name)
 })
 
@@ -57,30 +152,30 @@ for(i in 1:I){
   c1.mean[i] <-data_frames[[i]][5,5] 
   c2.mean[i] <-data_frames[[i]][6,5] 
   c3.mean[i] <-data_frames[[i]][7,5] 
-  cp1.mean[i] <-mean(data_frames[[i]][8:807,5])
-  pi1.mean[i] <-data_frames[[i]][808,5] 
-  pi2.mean[i] <-data_frames[[i]][809,5] 
-  pi1r.mean[i] <-data_frames[[i]][810,5] 
-  pi2r.mean[i] <-data_frames[[i]][811,5] 
+  cp1.mean[i] <-mean(data_frames[[i]][8:607,5])
+  pi1.mean[i] <-data_frames[[i]][608,5] 
+  pi2.mean[i] <-data_frames[[i]][609,5] 
+  pi1r.mean[i] <-data_frames[[i]][610,5] 
+  pi2r.mean[i] <-data_frames[[i]][611,5] 
   #u1.mean[i] <-mean(data_frames[[i]][812:1611,5])
-  u1.tau.inv.mean[i] <-data_frames[[i]][812,5]
+  u1.tau.inv.mean[i] <-data_frames[[i]][612,5]
   #u2.mean[i] <-mean(data_frames[[i]][1612:2011,5])
-  u2.tau.inv.mean[i] <-data_frames[[i]][813,5]
-  cp1.mu.mean[i] <-data_frames[[i]][816,5]
-  cp1.tau.mean[i] <-data_frames[[i]][817,5]
+  u2.tau.inv.mean[i] <-data_frames[[i]][613,5]
+  cp1.mu.mean[i] <-data_frames[[i]][616,5]
+  cp1.tau.mean[i] <-data_frames[[i]][617,5]
   
-  b10.mean[i] <-data_frames[[i]][819,5] 
-  b20.mean[i] <-data_frames[[i]][820,5] 
-  b1.mean[i] <-data_frames[[i]][821,5] 
-  b2.mean[i] <-data_frames[[i]][822,5] 
-  a.mean[i] <-data_frames[[i]][823,5] 
-  ga10.mean[i] <-data_frames[[i]][824,5] 
-  ga20.mean[i] <-data_frames[[i]][825,5] 
-  ga11.mean[i] <-data_frames[[i]][826,5] 
+  b10.mean[i] <-data_frames[[i]][619,5] 
+  b20.mean[i] <-data_frames[[i]][620,5] 
+  b1.mean[i] <-data_frames[[i]][621,5] 
+  b2.mean[i] <-data_frames[[i]][622,5] 
+  a.mean[i] <-data_frames[[i]][623,5] 
+  ga10.mean[i] <-data_frames[[i]][624,5] 
+  ga20.mean[i] <-data_frames[[i]][625,5] 
+  ga11.mean[i] <-data_frames[[i]][626,5] 
   #w1.mean[i] <-mean(data_frames[[i]][2027:2426,5])
-  w1.tau.inv.mean[i] <-data_frames[[i]][829,5]
+  w1.tau.inv.mean[i] <-data_frames[[i]][629,5]
   #w2.mean[i] <-mean(data_frames[[i]][2427:2826,5])
-  w2.tau.inv.mean[i] <-data_frames[[i]][830,5]
+  w2.tau.inv.mean[i] <-data_frames[[i]][630,5]
 }
 
 Sim.results=cbind(Flag,B1.mean,B2.mean,cp1.mean,c10.mean,c20.mean,c1.mean,c2.mean,c3.mean,pi1.mean,pi2.mean,pi1r.mean,pi2r.mean,
@@ -94,13 +189,6 @@ round(colMeans(Sim.results),2)
 
 
 
-
-
-
-
-
-setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM_020225")
-setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixJM_021125")
 setwd("/Users/Shared/Windows/UCHealth/RA/Project/EPIC-CF/Analysis_Jiayuan/EPIC_Sim_Results/mixnewJM2_031525")
 
 ###########################################################################
